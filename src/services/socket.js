1da930b9272d563b51b9200b9abea7c9470e8543
reyes200205 <jorgerenteriareyes4@gmail.com>
2026-04-24 13:16:44 -0600

    cambios

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Esta constante detecta si estás en producción o local automáticamente
-const SOCKET_URL = 'https://8586-187-189-15-231.ngrok-free.app';
+const SOCKET_URL = 'https://de72-187-189-15-231.ngrok-free.app';
 
 console.log("🔌 Conectando al socket en:", SOCKET_URL);
 
